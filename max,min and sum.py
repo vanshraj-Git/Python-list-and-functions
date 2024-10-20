12c91bb1394d768b5cbe68dfe06710125cd72918
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-10-21 04:45:54 +0530

    Add files via upload

---
+++ max,min and sum.py
@@ -0,0 +1,6 @@
+a=[1,2,3,4,5,6,-7,-8,-9]
+b=sum(a)  #sum of all elements
+c=max(a)  #largest no.
+d=min(a)  #smallest no.
+print("My list=",a,"\ntheir sum is",b)
+print("largest no. in the series=",c,"\nsmallest no. in the series=",d )
