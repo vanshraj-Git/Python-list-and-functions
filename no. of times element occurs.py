57f58b1e5d19f78d4a7341f3c004b23b61b44ef0
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-10-21 04:46:19 +0530

    Add files via upload

---
+++ no. of times element occurs.py
@@ -0,0 +1,5 @@
+a=[1,2,3,2,3,1,3,2,34,2,3,4,6,3,22,2,3,4,5,6,7,2]
+count1=a.count(1)
+count2=a.count(2)
+count3=a.count(3)
+print("My list=",a,"\nNo. of times element 1 appereared=",count1,'\nNo. of times elemet 2 appeared=',count2,"\nNo. of times element 3 appeared=",count3)
